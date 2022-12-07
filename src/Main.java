373f480d916dac21eb6665a82ca2e7563eadd0e6
Kawahara Ryusei <e225701@ie.u-ryukyu.ac.jp>
2022-12-08 03:05:36 +0900

    レポート5,ステップ2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main{
-
     public static void main(String[] args) {
-        String str1 = null;
-        System.out.print(str1.length());
+        try{
+            String str1 = null;
+            System.out.print(str1.length());
+        } catch (NullPointerException s ) { 
+            System.out.print("例外(NullPointerException)が発生しました。\n");
+            System.out.println(s);
+            System.out.println("getMessageの出力結果\n" + s.getMessage());
+        }        
     }
 } 
